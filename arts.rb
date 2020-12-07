f544c871b079ef48dfa846c3bec125635076fafc
Theosaurus-Rex <theosaurus13@gmail.com>
2020-12-08 10:47:30 +1100

    get questions and answers funtional for arts

--- arts.rb
+++ arts.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+require "tty-prompt"
 
 class ArtTrivia
     include HTTParty
@@ -10,8 +11,23 @@ class ArtTrivia
 end
 
 art_questions = ArtTrivia.new
+prompt = TTY::Prompt.new
 output = art_questions.questions
 output = output["results"]
 output.each do |q|
     puts q["question"]
+    answers = q["incorrect_answers"]
+    answers << q["correct_answer"]
+    answers = answers.shuffle
+    user_input = prompt.select("Select your answer:", answers)
+    if user_input == q["correct_answer"]
+        puts "Yes! You got it"
+        next output
+    else 
+        puts "Oops, that was incorrect"
+        next output
+    end
 end
+    
+
+
