b40149fe7d351134ea4c94d7030f2f017515a9d1
Theosaurus-Rex <theosaurus13@gmail.com>
2020-12-09 09:33:21 +1100

    split welcome into welcome and category select methods

--- index.rb
+++ index.rb
@@ -1,18 +1,23 @@
 require "tty-prompt"
 
 def welcome
-    categories = [
-        {name: "Art", value: 1},
-        {name: "General Knowledge", value: 2},
-    ]
-    prompt = TTY::Prompt.new
     intro = File.read('intro.txt')
     puts intro
     puts ""
     puts ""
     puts "Welcome to Terminal Trivia!"
+    
+end
+
+def category_select
+    categories = [
+        {name: "Art", value: 1},
+        {name: "General Knowledge", value: 2},
+    ]
+    prompt = TTY::Prompt.new
     puts "Please select a category to get started!"
     prompt.select("Which category?", categories)
 end
 
 welcome
+category_select
