c3cb625bc4c90527f2c3ee5a3855610ff7a32b8b
Theosaurus-Rex <theosaurus13@gmail.com>
2020-12-11 11:09:08 +1100

    docs update

--- art.rb
+++ art.rb
@@ -1,7 +1,5 @@
 require 'httparty'
 require "tty-prompt"
-require 'htmlentities'
-require './decoder'
 
 class ArtTrivia
     include HTTParty
@@ -14,18 +12,17 @@ class ArtTrivia
 end
 
 def art_quiz
-include Decoder
 art_questions = ArtTrivia.new
 prompt = TTY::Prompt.new
 output = art_questions.questions
 output = output["results"]
+p output
 output.each do |q|
     question = q["question"]
-    question = question.decode
+    question = question.gsub("&#039;", "'")
     puts question
     answers = q["incorrect_answers"]
     answers << q["correct_answer"]
-    answers.each {|a| a.decode}
     answers = answers.shuffle
     user_input = prompt.select("Select your answer:", answers)
     if user_input == q["correct_answer"]
