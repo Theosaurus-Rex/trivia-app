a3c2030129c1162239b2565ed47c125517be70c2
Theosaurus-Rex <theosaurus13@gmail.com>
2020-12-14 09:52:41 +1100

    add decoder gsubs to fix encoding bugs

--- decoder.rb
+++ decoder.rb
@@ -1,6 +1,6 @@
 
 module Decoder
     def decode
-        self.gsub("&uuml;", "ü").gsub("&eacute;", "é").gsub("&#039;", "'").gsub("&ldquo;", "\"").gsub("&quot;", "'").gsub("&rdquo", "”").gsub("&rsquo;", "’").gsub("&ocirc;", "ô").gsub("&amp;", "&").gsub("&ecirc;", "ê")
+        self.gsub("&uuml;", "ü").gsub("&eacute;", "é").gsub("&#039;", "'").gsub("&ldquo;", "\"").gsub("&quot;", "'").gsub("&rdquo", "”").gsub("&rsquo;", "’").gsub("&ocirc;", "ô").gsub("&amp;", "&").gsub("&ecirc;", "ê").gsub("&ntilde;", "~").gsub("&deg;", "°")
     end
 end
